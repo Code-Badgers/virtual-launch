931c1a22062cc20420d54f5a37a669359605b4a2
warrockhali <warrockhali@gmail.com>
2026-03-25 22:00:13 +0900

    feat: MemberRepository 인터페이스 생성

---
+++ backend/src/main/java/codebadger/virtual_launch/domain/member/domain/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package codebadger.virtual_launch.domain.member.domain.repository;
+
+import codebadger.virtual_launch.domain.member.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
