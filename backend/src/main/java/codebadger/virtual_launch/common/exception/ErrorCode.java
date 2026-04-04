7a4f1fb1ecfdb67fb918df33f69fdc733e92b118
warrockhali <warrockhali@gmail.com>
2026-04-04 23:57:32 +0900

    feat: Credit 관련 ErroCode 추가

--- backend/src/main/java/codebadger/virtual_launch/common/exception/ErrorCode.java
+++ backend/src/main/java/codebadger/virtual_launch/common/exception/ErrorCode.java
@@ -15,8 +15,9 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "M001", "존재하지 않는 회원입니다."),
     EMAIL_DUPLICATION(HttpStatus.BAD_REQUEST, "M002", "이미 사용 중인 이메일입니다."),
 
-    // 잔액
+    // 지갑
     LACK_OF_BALANCE(HttpStatus.BAD_REQUEST, "W001", "지갑 잔액이 부족합니다."),
+    INVALID_CREDIT_VALUE(HttpStatus.BAD_REQUEST, "W002", "유효하지 않은 금액입니다."),
 
     // 리뷰 크롤링
     CRAWLING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "CR001", "리뷰 크롤링에 실패했습니다."),
