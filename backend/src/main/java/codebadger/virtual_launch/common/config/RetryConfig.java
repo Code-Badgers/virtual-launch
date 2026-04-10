d1f8f579287ce07b41dffedb608198b1aeb754b5
warrockhali <warrockhali@gmail.com>
2026-04-10 19:10:13 +0900

    feat: RetryConfig 객체 구현

---
+++ backend/src/main/java/codebadger/virtual_launch/common/config/RetryConfig.java
@@ -0,0 +1,10 @@
+package codebadger.virtual_launch.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.resilience.annotation.Retryable;
+
+@Configuration
+@Retryable
+public class RetryConfig {
+
+}
