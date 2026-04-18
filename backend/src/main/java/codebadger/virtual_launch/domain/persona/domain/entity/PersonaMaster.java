b9c403648313a8ebde5ac161f4f48aea32bcfcb4
warrockhali <warrockhali@gmail.com>
2026-04-18 17:47:22 +0900

    refactor: PersonaMaster 엔티티의 필요없는 라이브러리 임포트 제거

--- backend/src/main/java/codebadger/virtual_launch/domain/persona/domain/entity/PersonaMaster.java
+++ backend/src/main/java/codebadger/virtual_launch/domain/persona/domain/entity/PersonaMaster.java
@@ -3,7 +3,6 @@ package codebadger.virtual_launch.domain.persona.domain.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Table(name = "persona_master")
 @NoArgsConstructor
